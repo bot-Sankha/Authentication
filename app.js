484f75e1acb206541d1a9e0ede322d174ad35d5d
bot-Sankha <57613327+bot-Sankha@users.noreply.github.com>
2020-05-06 19:21:13 +0530

    Second Commit - mongoose encryption added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption')
 
 const app = express();
 
@@ -11,10 +12,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
 mongoose.connect('mongodb://localhost:27017/secretsUserDB', {useNewUrlParser: true, useUnifiedTopology: true });
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email : String,
     password : String
-}
+});
+
+const secret = 'ThisistheMasterKey.'
+userSchema.plugin(encrypt, { secret : secret, encryptedFields: ['password'] });
+
 const userMod = mongoose.model('user', userSchema);
 
 
