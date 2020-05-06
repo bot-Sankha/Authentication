6df4d852dde5c2f5d6433029693647697b545560
bot-Sankha <57613327+bot-Sankha@users.noreply.github.com>
2020-05-06 21:07:03 +0530

    Fourth commit - Hashing function added to password using md5

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+// const encrypt = require('mongoose-encryption')
+const md5 = require('md5');
 
 const app = express();
 
@@ -18,7 +19,8 @@ const userSchema = new mongoose.Schema({
     password : String
 });
 
-userSchema.plugin(encrypt, { secret : process.env.SECRET, encryptedFields: ['password'] });
+// DB encryption using mongoose-encryption - plugin added, key passed fields specified
+// userSchema.plugin(encrypt, { secret : process.env.SECRET, encryptedFields: ['password'] });
 
 const userMod = mongoose.model('user', userSchema);
 
@@ -40,7 +42,7 @@ app.get('/login', (req,res) => {
 app.post('/register', (req, res) => {
     const newUser = new userMod({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err) => {
@@ -53,7 +55,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req,res) => {
     const uName = req.body.username;
-    const uPass = req.body.password;
+    const uPass = md5(req.body.password);
 
     userMod.findOne({email : uName}, (err, foundUser) => {
         if(err){
